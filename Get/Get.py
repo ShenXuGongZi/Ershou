15dc71cf65b1c3db3b8022058dcd1b276e90f4bc
shenxugongzi <liuhang0077@gmail.com>
2014-07-26 08:51:22 +0800

    最终更新

--- Get/Get.py
+++ Get/Get.py
@@ -3,8 +3,8 @@ from Model import *
 
 
 #if __name__ == '__main__':
-Ershou_game("http://bbs.tgbus.com/forum-50-2.html",'http://bbs.tgbus.com/','电玩巴士','dianwanbashi')
 Ershou_mydigit("http://bbs.mydigit.cn/thread.php?fid=73&page=2",'http://bbs.mydigit.cn/','数码之家','shumazhijia')
+Ershou_game("http://bbs.tgbus.com/forum-50-2.html",'http://bbs.tgbus.com/','电玩巴士','dianwanbashi')
 Ershou('http://bbs.feng.com/thread-htm-fid-29-page-2.html','http://bbs.feng.com/','威锋','weifeng')
 # Ershou('http://www.chiphell.com/forum-97-2.html','http://www.chiphell.com/','chiphell','Shuma')
 
